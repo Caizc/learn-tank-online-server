fca299b3e105a2051c37f55fe4281b64c48b343e
ZicongCai <dorightlife@163.com>
2017-09-01 18:11:52 +0800

    整理部分代码格式

--- Serv/core/Main.cs
+++ Serv/core/Main.cs
@@ -2,34 +2,38 @@ using System;
 
 namespace Serv
 {
-	class MainClass
-	{
+    class MainClass
+    {
         // TODO: Socket Server IP&Port
-	    private static string ip = "192.168.1.111";
-	    private static int port = 1234;
+        private static string IP = "192.168.1.111";
 
-		public static void Main(string[] args)
-		{
-			RoomMgr roomMgr = new RoomMgr ();
-			DataMgr dataMgr = new DataMgr ();
-			ServNet servNet = new ServNet();
-			servNet.proto = new ProtocolBytes ();
-			servNet.Start(ip, port);
+        private static int PORT = 1234;
 
-			while(true)
-			{
-				string str = Console.ReadLine();
-				switch(str)
-				{
-				case "quit":
-					servNet.Close();
-					return;
-				case "print":
-					servNet.Print();
-					break;
-				}
-			}
+        /// <summary>
+        /// 服务端程序入口
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            RoomMgr roomMgr = new RoomMgr();
+            DataMgr dataMgr = new DataMgr();
+            ServNet servNet = new ServNet();
+            servNet.proto = new ProtocolBytes();
+            servNet.Start(IP, PORT);
 
-		}
-	}
+            while (true)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "quit":
+                        servNet.Close();
+                        return;
+                    case "print":
+                        servNet.Print();
+                        break;
+                }
+            }
+        }
+    }
 }
